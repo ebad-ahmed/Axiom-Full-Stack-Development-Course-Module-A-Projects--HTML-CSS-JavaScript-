2969fa040bc481d743dccce032e11f85268c0d4e
ebad-ahmed <ebadahmed125@gmail.com>
2022-03-09 03:23:00 +0500

    Project 1 Completed
    completed all required functionalities

--- Project 1/script.js
+++ Project 1/script.js
@@ -27,9 +27,13 @@ function showSuccess(input) {
 }
 
 //function to check if email is valid ?
-function isValidEmail(email) {
+function checkEmail(input) { 
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim()) ) {
+        showSuccess(input);
+    } else {
+        showError(input,`Please provide a valid email`);
+    }
 }
 
 // function to check if required feilds have data
@@ -45,7 +49,26 @@ function checkRequired(inputArray) {
     });
 }
 
-//function to get the id of the input feild with proper case
+//Function to check length of input field
+function checkLength(input, min, max){
+    if (input.value.length < min ) {
+        showError(input,`${getFeildId(input)} needs to be atleast ${min} characters `);
+    } else if (input.value.length > max) {
+        showError(input,`${getFeildId(input)} needs to be less than ${max} characters `);
+    } else {
+        showSuccess(input);
+    }
+}
+
+//Function to check if password and confirm password match
+function checkPasswordMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2,"Passwords Don't Match ")
+    } 
+}
+
+
+//function to get the id of the input field with proper case
 function getFeildId(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -58,4 +81,8 @@ form.addEventListener('submit',function(e) {
     //console.log(username.value);
 
     checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordMatch(password,password2);
 });
