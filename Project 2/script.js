ee843aed7c5fca4542dadd4c6d512bc7a672ff99
ebad-ahmed <ebadahmed125@gmail.com>
2022-03-11 21:03:25 +0500

    Project 2 Completed

--- Project 2/script.js
+++ Project 2/script.js
@@ -1,65 +1,68 @@
+//Get DOM Elements
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-let ticketPrice = +movieSelect.value;
 
 populateUI();
 
-// Pull data from Local Storage to build UI
-function populateUI() {
-    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-    
-    if (selectedSeats !== null && selectedSeats.length > 0) {
-        seats.forEach( (seat, index) => {
-            if (selectedSeats.indexOf(index) > -1) {
-                seat.classList.add('selected');
-            }
-        });
-    }
+//+ is placed to covert strings to numeric value to calculate total
+let ticketPrice = +movieSelect.value;
 
-    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+//console.log(ticketPrice); 
 
-    if (selectedMovieIndex !== null) {
-        movieSelect.selectedIndex = selectedMovieIndex;
-    }
-
-}
-
-// Function to update counts
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    const countSelectedSeats = selectedSeats.length;
-
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    //console.log(seatsIndex); 
 
-    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
-        
-    count.innerText = countSelectedSeats;
-    total.innerText = ticketPrice * countSelectedSeats;
+    const selectedSeatsCount = selectedSeats.length; 
+    //console.log(selectedSeatsCount);
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount*ticketPrice;
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex)) 
 }
 
-// Function to save the selected movie and it's price
+//Save to the movie data to local storage
 function setMovieData(movieIndex,moviePrice) {
-    localStorage.setItem('selectedMovieIndex',movieIndex);
-    localStorage.setItem('selectedMoviePrice',moviePrice);
+    localStorage.setItem('selectedMovieIndex',movieIndex); 
+    localStorage.setItem('selectMoviePrice',moviePrice);
 }
 
-// Event Listener for Change on Select Movie Dropdown
-movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
-    setMovieData(e.target.selectedIndex, e.target.value);
-    updateSelectedCount();
-})
-
-// Event Listener for Click on Available Seats
-container.addEventListener('click', (e) => {
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected')
-        updateSelectedCount();
+//get data from local storage and populate ui
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null && selectedSeats > 0 ) {
+        seats.forEach((seat,index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+    };
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null ) {
+        movieSelect.selectedIndex = selectedMovieIndex ;
     }
+}
+
+//Event Listners
+//1.Event Listners for container to check for click on seats
+container.addEventListener('click', e => {
+   if(e.target.classList.contains('seat') && 
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();  
+    }
+});
+
+//2.Event listener for movie select
+movieSelect.addEventListener('change', e => {
+     ticketPrice = +e.target.value;
+     setMovieData (e.target.selectedIndex,e.target.value);
+     updateSelectedCount();
 })
 
-// Calculate initial number of seats and total price
+
 updateSelectedCount();
